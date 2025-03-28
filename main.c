e20ae63889b71d807efba3754b99a9fabdb45e12
NeyzinGameplays <120185784+NeyzinGameplays@users.noreply.github.com>
2025-03-28 14:13:26 -0300

    troca de if por switch case

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 
 int main() {
     printf("Bem-vindo ao Poker 2025.1!");
+    
     while(1){
         int entrada;
-        printf("Digite:\n 1 - Criar Jogador\n 2 - Sair do jogo.\n> ");
-        scanf("%d", &entrada);
-        if(entrada == 1){
-            jogador nUm;
+        jogador nUm;
             char nome[MAXIMO_NOME];
             int fichas;
             int posicaoMesa;
+        
+        printf("Digite:\n 1 - Criar Jogador\n 2 - Sair do jogo.\n> ");
+        scanf("%d", &entrada);
+        
+        switch(entrada){
+            case 1:
             printf("Digite o nome do jogador (serao salvos apenas 20 caracteres): ");
             scanf("%19s", nome);
             printf("Digite o numero de fichas que o jogador tera para comecar: ");
@@ -20,14 +24,15 @@ int main() {
             printf("Informe a posição do jogador na mesa: ");
             scanf("%d", &posicaoMesa);
             criarJogador(&nUm, nome, fichas, posicaoMesa);
-        }else if(entrada == 2){
+            break;
+            
+            case 2:
             printf("Jogo encerrado!");
             break;
-        }else{
+            
+            default:
             printf("Entrada incorreta, encerrando jogo!");
-            break;
         }
-        
-    }
-    return 0;
+    
+        return 0;
 }
