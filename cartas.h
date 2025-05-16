aab48222bcc4667e6823987e303917b2235867f0
NeyzinGameplays <120185784+NeyzinGameplays@users.noreply.github.com>
2025-05-16 14:25:41 -0300

    atualização de cartas.h

--- cartas.h
+++ cartas.h
@@ -4,49 +4,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "pilhaLE.h"
 
-#define NUMERO_DE_CARTAS 13
-#define NUMERO_DE_NAIPES 4
+// Constantes
+#define NUMERO_DE_CARTAS 13  // Número de valores de cartas (2-A)
+#define NUMERO_DE_NAIPES 4   // Número de naipes
+#define TOTAL_CARTAS 52      // Total de cartas no baralho
 
-void embaralha(Baralho *b, tp_pilha *p){
-    srand(time(NULL));
-    int ran =0;
+/**
+ * Embaralha as cartas usando o algoritmo Fisher-Yates
+ * @param b Baralho a ser embaralhado
+ * @param p Pilha onde as cartas serão colocadas
+ */
+void embaralha(Baralho *b, tp_pilha *p) {
+    srand(time(NULL)); // Inicializa semente aleatória
 
-    for (int i = 0; i < TOTAL_CARTAS; i++){
-        ran = rand() % 52;
-        Carta temp = b -> cartas[i];
-        b -> cartas[i] = b->cartas[ran];
-        b->cartas[ran] = temp;
-        push(p, b->cartas[i]);
+    // Algoritmo Fisher-Yates para embaralhar
+    for (int i = TOTAL_CARTAS - 1; i > 0; i--) {
+        int j = rand() % (i + 1); // Escolhe índice aleatório
+        // Troca as cartas
+        Carta temp = b->cartas[i];
+        b->cartas[i] = b->cartas[j];
+        b->cartas[j] = temp;
     }
 
+    // Empilha as cartas embaralhadas
+    for (int i = 0; i < TOTAL_CARTAS; i++) {
+        push(p, b->cartas[i]);
+    }
 }
 
-void inicializaBaralho(Baralho *b){
-    char *valores[NUMERO_DE_CARTAS] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-
-    char *naipes[NUMERO_DE_NAIPES] = {"Copas", "Ouros", "Espadas", "Paus"};
-
+/**
+ * Inicializa um baralho com todas as cartas
+ * @param b Baralho a ser inicializado
+ */
+void inicializaBaralho(Baralho *b) {
+    const char *valores[] = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+    const char *naipes[] = {"Copas","Ouros","Espadas","Paus"};
 
     int index = 0;
-    
+    // Cria todas as combinações de valores e naipes
     for (int i = 0; i < NUMERO_DE_NAIPES; i++) {
         for (int j = 0; j < NUMERO_DE_CARTAS; j++) {
-            b -> cartas[index].valor = valores[j];
-            b -> cartas[index].naipe = naipes[i];
+            b->cartas[index].valor = (char*)valores[j];
+            b->cartas[index].naipe = (char*)naipes[i];
             index++;
         }
     }
-
-
 }
 
-void imprimirBaralho(Baralho b){
+/**
+ * Imprime todas as cartas do baralho (para debug)
+ * @param b Baralho a ser impresso
+ */
+void imprimirBaralho(Baralho b) {
     for (int i = 0; i < TOTAL_CARTAS; i++) {
         printf("%s de %s\n", b.cartas[i].valor, b.cartas[i].naipe);
-  }
+    }
 }
 
 #endif
