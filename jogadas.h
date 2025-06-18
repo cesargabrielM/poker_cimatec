20224f1771762ad22f768025e2ee127fc1b2b7e4
CaueTech <cauemine10@gmail.com>
2025-06-18 14:23:35 -0300

    Update jogadas.h

--- jogadas.h
+++ jogadas.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arvore_avl.h"
+#include "arvAvl_dict.h"
 
 /* 
 COMO USAR ESSA BIBLIOTECA?
@@ -9,64 +9,58 @@ Primeiro de tudo, ao começo da cada partida, usamos a função
 inicializaArquivo(). 
 Depois disso, usamos criaAVL() para criar a árvore AVL que 
 armazenas as jogadas.
-*/
-
-/*
-Essa função abre o arquivo onde serão registradas as jogadas.
+Depois disso, a cada jogada, usamos insereJogada() passando o
+ponteiro da raiz da árvore, o nome do jogador que fez a jogada,
+essa função abre o arquivo onde serão registradas as jogadas.
 SÓ PODE SER USADA NO INÍCIO DE CADA PARTIDA, SENÃO APAGA TUDO QUE
 FOI SALVO. Logo após criar o arquivo, ela fecha automaticamente.
 O arquivo só é aberto novamente na função salvaEmDisco() no modo
 apêndice.
 A cada jogada, usamos insereJogada(), com os respectivos parâmetros
 para salvar na árvore.
-Ao usar insereJogada(), ela automaticamente usa salvaEmDisco() para
-registrar no arquivo de texto.
+Depois disso, quando a jogada for feita, usamos a função
+salvaEmDisco() passando o ponteiro da raiz da árvore e a ordem da jogada.
 */
-void inicializaArquivo(){
+
+int inicializaArquivo(){
     FILE *f = fopen("jogadas.txt", "w");
+    if(f == NULL) return 0; //Se não conseguiu abrir o arquivo, retorna 0
     fclose(f);
+    return 1; //Se conseguiu abrir e fechar o arquivo, retorna 1
 }
 
-/*
-Essa função cria a árvore AVL onde serão armazenadas as jogadas, 
-retorna o ponteiro dela.
-*/
 arvAvl * criaAVL(){
     arvAvl *p_raiz = criarAvl();
     return p_raiz;
 }
 
-/*
-Essa função insere um nó na árvore AVL de jogadas, ela precisa receber o ponteiro da 
-raiz da árvore, o nome do jogador que a fez (recebe o ponteiro para o primeiro 
-caractere da string), a ordem da jogada e o título. Após inserir, ela automaticamente
-chama a função salvaEmDisco()
-*/
-void insereJogada(arvAvl *p_raiz, char* nome, int ordem, tipos_jogada titulo){
+int insereJogada(arvAvl *p_raiz, char* nome, int ordem, tipos_jogada titulo){
     arvAvl ultima_jogada;
     ultima_jogada = inserir(p_raiz, ordem, nome, titulo);
-    if(ultima_jogada != NULL) salvaEmDisco(ultima_jogada);
-    //Somente insere se for possível adicionar a jogada na árvore
+    if(ultima_jogada == NULL) return 0; //Se não conseguiu inserir, retorna 0
+    return 1; //Se conseguiu inserir, retorna 1
 }
 
-/*
-A função que eu vou criar agora funciona da seguinte forma: assim que um jogador
-fizer uma jogada, ela deve ser chamada. Ela precisa receber o ponteiro da última
-jogada feita, e que o arquivo de registro tenha sido criado e fechado.
-*/
-void salvaEmDisco(arvAvl ultima_jogada){
+int salvaEmDisco(arvAvl* p_raiz, int ordem){
     FILE *f = fopen("jogadas.txt", "a"); //Abre o arquivo no modo apêndice para registrar
     
     if(f == NULL){
         printf("Erro ao acessar o arquivo!");
-        return;
-    }; //Tenta acessar o arquivo
+        fclose(f);
+        return 0;
+    } //Tenta acessar o arquivo
 
-    tipos_jogada t = ultima_jogada->titulo; //Salva o título da última jogada
+    arvAvl jogada_encontrada = consultarValorAvl(p_raiz, ordem); //Tenta achar a jogada pela ordem
+    if(jogada_encontrada == NULL){
+        fclose(f); //Se não conseguiu achar, fecha o arquivo
+        return 0; //E retorna 0
+    }
+
+    tipos_jogada t = jogada_encontrada->titulo; //Salva o título da jogada encontrada
     
     char nomeJogada[MAX_NOME]; //Cria uma string para salvar o nome da jogada
     char nomeJogador[MAX_NOME]; //Cria uma string para salvar o nome do jogador
-    strcpy(nomeJogador, ultima_jogada->nome_jogador);
+    strcpy(nomeJogador, jogada_encontrada->nome_jogador);
     
     //Switch Case, para cada tipo de jogada, ele imprime a string correspondente
     switch (t) {
@@ -116,4 +110,6 @@ void salvaEmDisco(arvAvl ultima_jogada){
             break;
     }
     fclose(f);
+
+    return 1; //Se conseguiu salvar, retorna 1
 }
